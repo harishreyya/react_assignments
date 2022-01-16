63e0ced250e5bb5132de920b03b7d0856b704204
harishreyya <harishrebel0@gmail.com>
2022-01-16 21:06:05 +0530

    auth context

---
+++ auth_context/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Navbar} from './components/Navbar'
+function App() {
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;

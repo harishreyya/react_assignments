31601ffdae7b177d2a54c5ea0fb06b8d6d19e7fb
harishreyya <harishrebel0@gmail.com>
2022-01-20 01:44:22 +0530

    forms

---
+++ forms/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import {Show} from "./components/Show"
+
+function App() {
+
+  return (
+    <div className="App">
+ <h1>User Details Form</h1>
+ 
+   < Show />
+   
+
+    </div>
+  );
+}
+
+export default App;
---
+++ forms/src/components/Show.jsx
@@ -0,0 +1,46 @@
+import {Form} from './form'
+import {Table} from './table'
+import {useEffect, useState } from 'react'
+ 
+export const  Show = () =>{
+    const [formData, setFormData] = useState([])
+
+
+    const handleSubmit = (data) =>{
+      
+        fetch("http://localhost:3001/forms",{
+            "method" : "POST",
+            "headers" : {
+                "content-type" :"application/json"
+            },
+            "body" : JSON.stringify(data)
+        })
+        .then(res=>res.json())
+        .then(result => {
+            console.log(result)
+            handleGet()
+        })
+    }
+
+    useEffect(() => {
+        handleGet()
+       
+    }, [])
+   
+    const handleGet =()=>{
+        fetch("http://localhost:3001/forms")
+        .then(res=>res.json())
+        .then(result => {
+            setFormData(result)
+        })
+        console.log('formData', formData)
+    }
+
+    return(
+        <>
+         < Table getdata = {formData} />
+       < Form postData = {handleSubmit} />
+      
+        </>
+    )
+}

9848acd62ecfed0422089603821be05533cc3f8c
harishreyya <harishrebel0@gmail.com>
2022-02-06 23:14:04 +0530

    todo

---
+++ todo/src/components/Todos.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+
+
+export const Todos =()=>{
+
+    const [todos,setTodos] = useState([]);
+    const [text,setText] = useState("");
+    const [loading,setLoading] = useState(false)
+    const [page,setPage] = useState(1)
+  useEffect(()=>{
+      getData()
+    },[page])
+
+    const getData = () =>{
+        setLoading(true)
+        fetch(`http://localhost:3001/todos?_page=${page}&_limit=3`).then(d=>d.json()).then(res=>{
+            setTodos(res)
+            setLoading(false)
+        })
+    }
+    return loading ? ("loading..." 
+    ): (
+    <div>
+        <input 
+        placeholder="enter todos" 
+        onChange={(e) => setText(e.target.value)}
+        />
+       
+        <button onClick={() => {
+            const data = {status : false ,title: text}
+            fetch("http://localhost:3001/todos",{
+                method:"POST",
+                body: JSON.stringify(data),
+                headers:{
+                    "content-type":"application/json"
+                },
+            }).then(getData)
+            //   setTodos([...todos,{status : false ,title: text}])
+        }} > Add todos </button> 
+       
+        {todos.map(e => <div key={e.id}>{e.title} - {e.status ? "done"  : "not done" }</div>)} 
+
+        <button onClick={()=>{
+            setPage(page - 1)
+        }}
+        >prev</button>
+        <button onClick={()=>{
+            setPage(page + 1)
+        }}
+        >next</button>
+        </div>
+
+        ) 
+   
+}

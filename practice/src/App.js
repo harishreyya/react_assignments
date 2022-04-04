704e547255859b1bcb68a4d946cc4c6799ebe3d6
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-04-04 22:55:35 +0530

    practice counter

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const[state,setState]=useState(0)
+// const increment = ()=>{
+//   setState(state + 1)
+// }
+
+if(state==25)
+return <div>reached maximum</div>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <h1>{state}</h1>
+      <button onClick={()=>setState(state+1)}>button</button>
     </div>
   );
 }

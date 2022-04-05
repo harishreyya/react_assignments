494aa9138e9db26cf8135fac06b4979e8f268fec
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-04-05 23:26:25 +0530

    push

--- comments/src/App.js
+++ comments/src/App.js
@@ -3,16 +3,14 @@ import './App.css';
 
 
 function App() {
-  
-
-
-
-
+  //normal search
   return (
     <div className="App">
-    
-   
-
+    <div className="flex">
+   <input className="search" />
+   <img src="https://thumbs.dreamstime.com/b/mike-mike-icon-146784248.jpg"/>
+   <img src="https://previews.123rf.com/images/vasiffeyzullazadeh/vasiffeyzullazadeh1904/vasiffeyzullazadeh190401037/122815761-search-icon-isolated-on-white-background-search-icon-in-trendy-design-style-search-vector-icon-moder.jpg"/>
+   </div>
     </div>
   );
 }

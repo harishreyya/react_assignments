1a2569f4471b09b8a2819cbbc2e0032933ab26be
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-04-05 22:36:25 +0530

    rsvbwv

--- comments/src/App.js
+++ comments/src/App.js
@@ -1,40 +1,17 @@
 import React, {useState, useEffect} from "react"
 import './App.css';
 
-const fetchURL = "https://fast-reef-22226.herokuapp.com/data"
-function App() {
-  const [data, setData] = useState(null)
-  const getData = () =>
-    fetch(`${fetchURL}`)
-      .then((res) => res.json())
 
- useEffect(() => {
-  console.log(data)
-    getData().then((data) => setData(data))
+function App() {
   
-  }, [])
 
 
 
 
   return (
     <div className="App">
-     
-     <h1>all data shown here</h1>
-      <div>
-  {data?.map((item) => 
-    <div className="show" >
-    <h5><strong>title:</strong>{item.WebName}</h5>
-    <hr/>
-    </div>
+    
    
-  )}
-  
-   </div>
-
-
-   <input onChange={e.target.value} placeholder="kuch bhi likho"/>
-   <input type="submit"/>
 
     </div>
   );

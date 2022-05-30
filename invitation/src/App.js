4fa2fd33dcf4b25d77c012726fce5df9ee6f044e
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-05-30 22:39:35 +0530

    invitation

---
+++ invitation/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Invitation } from './components/invitation';
+
+function App() {
+  return (
+    <div className="App">
+      
+<Invitation/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ invitation/src/components/invitation.jsx
@@ -0,0 +1,35 @@
+import "./invitation.css"
+
+export const Invitation = () => {
+
+    return <div >
+<div className=" box">
+    <br />
+    <br />
+    <br />
+
+    <br />
+    <br />
+    <br />
+
+    
+<p className="quote"> you are invited on the auspicious </p>
+<p className="quote">occasion of wedding ceremony  of</p>
+<p className="name"> <b><i>Deepthi</i> </b> </p>
+<p className="name">&</p>
+<p className="name"><b> <i> Dharma rao</i> </b></p>
+<div className="venue">Lunch on 7<sup>th</sup>June at Kolluru Village</div>
+<div className="venue">Wedding</div>
+ <div></div>   
+
+
+
+<img  className="pic" src="https://www.kindpng.com/picc/m/120-1201748_kannada-couple-invitation-design-wedding-cartoon-couple-png.png"/>
+
+
+</div>
+
+
+
+    </div>
+}

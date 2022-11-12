faf90e3a68823074b542505a6c675321421e3d48
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-11-12 18:45:38 +0530

    htd

--- practice/src/components/counter.js
+++ practice/src/components/counter.js
@@ -10,14 +10,15 @@ export const Counter =()=>{
     if(state==25)
     return <div>reached maximum</div>
       return (
-        <div >
+        // <div >
          
-          <h1>{state}</h1>
-          <button onClick={()=>setState(state+1)}>button +</button>
-          <button onClick={()=>setState(state-1)}>button -</button>
-          <button onClick={()=>setState(0)}>reset</button>
+        //   <h1>{state}</h1>
+        //   <button onClick={()=>setState(state+1)}>button +</button>
+        //   <button onClick={()=>setState(state-1)}>button -</button>
+        //   <button onClick={()=>setState(0)}>reset</button>
     
          
-        </div>
+        // </div>
+        <span>hi</span>
       );
 }

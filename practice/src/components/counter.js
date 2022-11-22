aab1dcf90d33601957d970471efc6bbafd3a2644
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-11-22 22:42:55 +0530

    JUFU

--- practice/src/components/counter.js
+++ practice/src/components/counter.js
@@ -3,9 +3,9 @@ import { useState } from 'react';
 
 export const Counter =()=>{
     const[state,setState]=useState(0)
-    // const increment = ()=>{
-    //   setState(state + 1)
-    // }
+    const increment = ()=>{
+      setState(state + 1)
+    }
     
     if(state==25)
     return <div>reached maximum</div>
